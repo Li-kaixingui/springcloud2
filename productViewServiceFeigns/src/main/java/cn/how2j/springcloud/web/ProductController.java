c97cc3036d2484541411d0e262add05266dbcc08
likaixin <244208180@qq.com>
2020-10-14 17:21:54 +0800

    test2

--- productViewServiceFeigns/src/main/java/cn/how2j/springcloud/web/ProductController.java
+++ productViewServiceFeigns/src/main/java/cn/how2j/springcloud/web/ProductController.java
@@ -3,6 +3,7 @@ package cn.how2j.springcloud.web;
 import cn.how2j.springcloud.pojo.Product;
 import cn.how2j.springcloud.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +14,13 @@ import java.util.List;
 public class ProductController {
     @Autowired
     ProductService productService;
+    @Value("${version}")
+    String version;
 
     @RequestMapping("/products")
     public Object products(Model m) {
         List<Product> ps = productService.listProducts();
+        m.addAttribute("version", version);
         m.addAttribute("ps", ps);
         return "products";
     }
